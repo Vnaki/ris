78ed021191d2629b11849bd236150704435e7fa1
wuquanyao <wqynqa@163.com>
2023-02-10 15:03:39 +0800

    fix sqlite connection

--- components/database/sqlite.go
+++ components/database/sqlite.go
@@ -19,11 +19,13 @@ func (c *SQLiteDatabase) Connect() (db.Session, error) {
 	// options @see https://github.com/mattn/go-sqlite3#connection-string
 	conn := sqlite.ConnectionURL{
 		Database: c.Database,
-		Options:  map[string]string{
-			"mode": "rw", // 数据库访问模式, ro/rw/rwc/memory
-			"cache": "private", // 缓存模式, shared or private
-			"_busy_timeout": "5000", // 处理超时, 单位毫秒
-			"_txlock": "deferred", // 事务锁, deferred/immediate/exclusive
+		Options: map[string]string{
+			"mode":          "rwc", // 数据库访问模式, ro/rw/rwc/memory
+			"_locking_mode": "NORMAL",
+			"_mutex":        "no",
+			"cache":         "private",  // 缓存模式, shared or private
+			"_busy_timeout": "5000",     // 处理超时, 单位毫秒
+			"_txlock":       "deferred", // 事务锁, deferred/immediate/exclusive
 		},
 	}
 
